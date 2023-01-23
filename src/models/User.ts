43930d27f13303e8e06107363a2bec9a27f394c8
skinnyfads <skinnyfads@proton.me>
2023-01-23 14:41:37 +0700

    feat(api): add username to user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,6 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   pictureUrl: { type: String },
   thumbnailUrl: { type: String },
